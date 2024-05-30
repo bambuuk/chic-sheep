90bae4e1e5406a567e1b483b2a8086f78943c7b5
ivan_korobka <vanyok.korobka@ukr.net>
2024-05-30 21:36:40 +0300

    upd layout in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { Box, styled } from "@mui/material";
+import { styled } from "@mui/material";
 import { CustomContainer } from "./CustomElements";
 import Logo from "./Logo";
 import Navigation from "./Navigation";
-import Link from "next/link";
 import { SocialNetworks } from "./SocialNetworks";
 
 const FooterUI = styled("footer")(({ theme }) => ({
@@ -23,13 +22,10 @@ const FooterUI = styled("footer")(({ theme }) => ({
 }));
 
 const Wrapper = styled("div")(({ theme }) => ({
-  padding: "482px 0 52px 0",
+  padding: "60px 0 52px 0",
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
-  [theme.breakpoints.down(1280)]: {
-    padding: "400px 0 52px 0",
-  },
   [theme.breakpoints.down(1024)]: {
     padding: "70.5px 0 100px 0",
     flexDirection: "column",
@@ -39,36 +35,6 @@ const Wrapper = styled("div")(({ theme }) => ({
   [theme.breakpoints.down(576)]: {
     padding: "60px 0 90px 0",
     gap: "40px",
-  },
-}));
-
-const NetworkList = styled(Box)(({ theme }) => ({
-  display: "flex",
-  gap: "45px",
-  alignItems: "center",
-  [theme.breakpoints.down(1280)]: {
-    gap: "30px",
-  },
-  [theme.breakpoints.down(1024)]: {
-    gap: "60px",
-  },
-  [theme.breakpoints.down(576)]: {
-    gap: "50px",
-  },
-}));
-
-const NetworkLink = styled(Link)(({ theme }) => ({
-  WebkitTapHighlightColor: "rgba(0, 0, 0, 0)",
-  "& path": {
-    transition: "0.4s all",
-  },
-  "&:hover path": {
-    fill: "#FBB41A",
-  },
-  [theme.breakpoints.down(1024)]: {
-    "&:hover path": {
-      fill: "white",
-    },
   },
 }));
 
