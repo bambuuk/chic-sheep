46c4cffd47ebbd3cf2bab4281736de228da0cf43
ivan_korobka <vanyok.korobka@ukr.net>
2024-06-27 17:09:17 +0300

    add link with font to Head component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Chic Sheep",
@@ -20,6 +21,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="../../public/fonts/Feogra-Demo-BF64dc66c9ef4b4.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body className={`${inter.className}`}>{children}</body>
     </html>
   );
